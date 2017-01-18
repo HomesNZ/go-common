bfaf8d7dbb71852e18dd5da4b83bfc82b4241582
Dylan Macdonald <dylan4823@gmail.com>
2017-01-19 11:21:21 +1300

    Add Missing parameters error

--- request/mandatory_parameters.go
+++ request/mandatory_parameters.go
@@ -2,6 +2,9 @@ package request
 
 import "net/http"
 
+//ErrorMissingRequiredParameters is returned when a request does not contain required parameters
+var ErrorMissingRequiredParameters error
+
 //ValidParameters determines whether the given request contains a value for every parameter in params
 func ValidParameters(req *http.Request, params []string) bool {
 	p := req.URL.Query()
