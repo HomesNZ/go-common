00a4e1f21c6f74b812d9209f55480d77053c0687
Dylan Macdonald <dylan.macdonald@homes.co.nz>
2017-01-19 12:40:04 +1300

    Return an error if the params aren't fulfilled

--- request/mandatory_parameters.go
+++ request/mandatory_parameters.go
@@ -2,17 +2,14 @@ package request
 
 import "net/http"
 
-//ErrorMissingRequiredParameters is returned when a request does not contain required parameters
-var ErrorMissingRequiredParameters error
-
 //ValidParameters determines whether the given request contains a value for every parameter in params
-func ValidParameters(req *http.Request, params []string) bool {
+func ValidParameters(req *http.Request, params []string) error {
 	p := req.URL.Query()
 
 	for _, parameter := range params {
 		if p.Get(parameter) == "" {
-			return false
+			return new ErrorMissingRequiredParameters error
 		}
 	}
-	return true
+	return nil
 }
