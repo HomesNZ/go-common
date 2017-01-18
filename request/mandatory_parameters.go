d4d57c6a20d2b9457c0eb710a807ed84be291a9f
Dylan Macdonald <dylan.macdonald@homes.co.nz>
2017-01-19 11:16:08 +1300

    Check for mandatory parameters

---
+++ request/mandatory_parameters.go
@@ -0,0 +1,15 @@
+package request
+
+import "net/http"
+
+//ValidParameters determines whether the given request contains a value for every parameter in params
+func ValidParameters(req *http.Request, params []string) bool {
+	p := req.URL.Query()
+
+	for _, parameter := range params {
+		if p.Get(parameter) == "" {
+			return false
+		}
+	}
+	return true
+}
