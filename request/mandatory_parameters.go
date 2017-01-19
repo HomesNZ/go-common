6b37e43e2afbddbf9359bc4b7c99d7a100e183a4
Dylan Macdonald <dylan.macdonald@homes.co.nz>
2017-01-19 14:32:57 +1300

    Return a proper error

--- request/mandatory_parameters.go
+++ request/mandatory_parameters.go
@@ -1,16 +1,16 @@
 package request
 
-import "net/http"
-
-var errorMissingRequiredParameters error
+import (
+	"fmt"
+	"net/http"
+)
 
 //ValidParameters determines whether the given request contains a value for every parameter in params
 func ValidParameters(req *http.Request, params []string) error {
 	p := req.URL.Query()
-
 	for _, parameter := range params {
 		if p.Get(parameter) == "" {
-			return errorMissingRequiredParameters
+			return fmt.Errorf("Invalid Parameters")
 		}
 	}
 	return nil
