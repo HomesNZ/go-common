4dcf5fba543e9ff3de3380ca7906b4205c498968
Dylan Macdonald <dylan.macdonald@homes.co.nz>
2018-03-20 10:33:05 +1300

    Mailer object

--- email/emailer.go
+++ email/emailer.go
@@ -1,8 +1,9 @@
 package email
 
 import (
+	"github.com/Sirupsen/logrus"
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ses"
 )
@@ -10,13 +11,16 @@ import (
 // CharSet is a default AWS character set
 const CharSet = "UTF-8"
 
-// NewSession initialises an AWS session
-func NewSession(sesRegion string) (*session.Session, error) {
-	return session.NewSession(
-		&aws.Config{
-			Region:      aws.String(sesRegion),
-			Credentials: credentials.NewEnvCredentials(),
-		})
+type Mailer struct {
+	Session *session.Session
+}
+
+// NewClient creates a new AWS session from conf and returns a Mailer object
+func NewClient(conf *aws.Config) (*Mailer, error) {
+	s, err := session.NewSession(conf)
+	return &Mailer{
+		Session: s,
+	}, err
 }
 
 // Email represents a very basic email structure
@@ -28,9 +32,9 @@ type Email struct {
 	Body        string
 }
 
-// Send sends a simple email via a smtp gateway using TLS
-func (e *Email) Send(sess *session.Session) error {
-	svc := ses.New(sess)
+// Send sends a simple email via AWS SES
+func (m *Mailer) Send(e Email) error {
+	svc := ses.New(m.Session)
 
 	input := &ses.SendEmailInput{
 		Source: aws.String(e.From),
@@ -53,4 +57,50 @@ func (e *Email) Send(sess *session.Session) error {
 	}
 	_, err := svc.SendEmail(input)
 	return err
+}
+
+// MustSend sends a simple email via AWS SES
+// MustSend will log any errors that occur, but they will not be returned
+func (m *Mailer) MustSend(e Email) {
+	svc := ses.New(m.Session)
+
+	input := &ses.SendEmailInput{
+		Source: aws.String(e.From),
+		Destination: &ses.Destination{
+			// CcAddresses: e.CCAddresses,
+			ToAddresses: e.To,
+		},
+		Message: &ses.Message{
+			Body: &ses.Body{
+				Text: &ses.Content{
+					Charset: aws.String(CharSet),
+					Data:    aws.String(e.Body),
+				},
+			},
+			Subject: &ses.Content{
+				Charset: aws.String(CharSet),
+				Data:    aws.String(e.Subject),
+			},
+		},
+	}
+	_, err := svc.SendEmail(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case ses.ErrCodeMessageRejected:
+				logrus.WithError(aerr).WithField("Error Code", ses.ErrCodeMessageRejected).Error()
+			case ses.ErrCodeMailFromDomainNotVerifiedException:
+				logrus.WithError(aerr).WithField("Error Code", ses.ErrCodeMailFromDomainNotVerifiedException).Error()
+			case ses.ErrCodeConfigurationSetDoesNotExistException:
+				logrus.WithError(aerr).WithField("Error Code", ses.ErrCodeConfigurationSetDoesNotExistException).Error()
+			default:
+				logrus.WithError(aerr).Error()
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			logrus.WithError(err).Error()
+		}
+	}
+	return
 }
