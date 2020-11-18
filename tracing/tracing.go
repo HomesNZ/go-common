4fd0e1f5e2e81008dffa5039a1402ab38819354d
Dylan Macdonald <dylan.macdonald@homes.co.nz>
2020-11-18 13:18:04 +1300

    Update default transport better

--- tracing/tracing.go
+++ tracing/tracing.go
@@ -51,7 +51,7 @@ func InitTracer(ctx context.Context, cfg *TracerConfig, sampleType trace.Sampler
 
 	global.SetTextMapPropagator(otel.NewCompositeTextMapPropagator(propagators.TraceContext{}, propagators.Baggage{}))
 	http.DefaultClient = &http.Client{Transport: otelhttp.NewTransport(http.DefaultTransport)}
-	http.DefaultTransport = &otelhttp.NewTransport(http.DefaultTransport)
+	http.DefaultTransport = http.DefaultClient.Transport
 
 	tr := global.Tracer("init")
 	_, span := tr.Start(ctx, "init")
