8e254a396d7840245cba61f3c6589a81fe590e6d
Igor Rashyn <igor.rashyn@homes.co.nz>
2020-11-25 12:05:38 +1300

    version v3 was added

---
+++ migrate/v3/go.mod
@@ -0,0 +1,16 @@
+module github.com/HomesNZ/go-common/migrate/v3
+
+go 1.15
+
+require (
+	github.com/HomesNZ/go-common/dbclient/v3 v3.0.0-20201124224420-d7be1020695b
+	github.com/HomesNZ/go-common/env v0.0.0-20201124200925-c819d04fe3dd
+	github.com/gomodule/redigo v1.8.3
+	github.com/jackc/pgx v3.6.2+incompatible
+	github.com/mna/redisc v1.1.7
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.7.0
+	golang.org/x/crypto v0.0.0-20201124201722-c8d3bf9c5392 // indirect
+	golang.org/x/text v0.3.4 // indirect
+	gopkg.in/jackc/pgx.v3 v3.6.2
+)
---
+++ migrate/v4/migrate_test.go
@@ -0,0 +1,127 @@
+package migrate
+
+import (
+	"context"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"github.com/HomesNZ/go-common/dbclient/v4"
+	"testing"
+)
+
+func TestCreatingMigratorWhenTableExists(t *testing.T) {
+	ctx := context.Background()
+
+	cfg := dbclient.Config{
+		MaxConns: 1,
+		Host: "localhost",
+		Name: "postgres",
+		User: "postgres",
+		Port: 5432,
+	}
+	adapter := Postgres{SchemaName: "test"}
+		path := fmt.Sprintf("test_migrations/")
+	db, _ := dbclient.Conn(ctx, &cfg)
+	m, err := NewMigrator(ctx, db, adapter, path, nil)
+	if err != nil {
+		t.Error(err)
+	}
+	err = m.Migrate(ctx, log.WithField("startup", "migrate"))
+	row := db.QueryRow(ctx, "select migration_id from test.schema_version")
+	var id uint64
+	err = row.Scan(&id)
+	if err != nil {
+		t.Error(err)
+	}
+	if id != 1 {
+		t.Error("Invalid id found in database")
+	}
+	//cleanup()
+}
+
+//func TestMigrationAndRollback(t *testing.T) {
+//	m := GetMigrator("test1")
+//
+//	if err := m.Migrate(); err != nil {
+//		t.Error(err)
+//	}
+//
+//	// Ensure that the migration ran.
+//	row := db.QueryRow(
+//		adapter.SelectMigrationTableSql(),
+//		"test",
+//	)
+//	var tableName string
+//	if err := row.Scan(&tableName); err != nil {
+//		t.Error(err)
+//	}
+//	if tableName != "test" {
+//		t.Errorf("Migration table not created")
+//	}
+//	// Ensure that the migrate status is correct.
+//	row = db.QueryRow(
+//		adapter.GetMigrationSql(),
+//		1,
+//	)
+//	var status int
+//	if err := row.Scan(&status); err != nil {
+//		t.Error(err)
+//	}
+//	if status != Active || m.migrations[1].Status != Active {
+//		t.Error("Invalid status for migration")
+//	}
+//	if err := m.RollbackN(len(m.migrations)); err != nil {
+//		t.Error(err)
+//	}
+//
+//	// Ensure that the down migration ran.
+//	row = db.QueryRow(
+//		adapter.SelectMigrationTableSql(),
+//		"test",
+//	)
+//	err := row.Scan(&tableName)
+//	if err != nil && err != sql.ErrNoRows {
+//		t.Errorf("Migration table should be deleted: %v", err)
+//	}
+//
+//	// Ensure that the migration log is missing.
+//	row = db.QueryRow(
+//		adapter.GetMigrationSql(),
+//		1,
+//	)
+//	if err := row.Scan(&status); err != nil && err != sql.ErrNoRows {
+//		t.Error(err)
+//	}
+//	if m.migrations[1].Status != Inactive {
+//		t.Errorf("Invalid status for migration, expected: %d, got: %v", Inactive, m.migrations[1].Status)
+//	}
+//
+//	cleanup()
+//}
+
+//func cleanup() {
+//	ctx :=context.Background()
+//	_, err := db.Exec(ctx, "drop table schema_version")
+//	if err != nil {
+//		panic(err)
+//	}
+//}
+
+//func init() {
+//	var err error
+//	dbType = "pg"
+//	ctx := context.Background()
+//	log.Print("Using postgres")
+//	//adapter = Postgres{}
+//	//db, err = sql.Open("postgres", "host=localhost dbname=gomigrate sslmode=disable")
+//
+//	db, err = dbclient.Conn(ctx, &dbclient.Config{
+//		MaxConns: 1,
+//		Host: "localhost",
+//		Name: "postgres",
+//		User: "postgres",
+//	})
+//
+//	if err != nil {
+//		panic(err)
+//	}
+//}
