39721f9324ab6985659cf196c253386df028743e
Igor Rashyn <igor.rashyn@homes.co.nz>
2020-11-24 15:45:59 +1300

    added v3 and v4 to dblient

---
+++ dbclient/v3/go.mod
@@ -0,0 +1,17 @@
+module github.com/HomesNZ/go-common/dbclient/v3
+
+go 1.15
+
+require (
+	github.com/HomesNZ/go-common/env v0.0.0-20201124005429-a7ff8dcf8c7c
+	github.com/cockroachdb/apd v1.1.0 // indirect
+	github.com/gofrs/uuid v3.3.0+incompatible // indirect
+	github.com/jackc/fake v0.0.0-20150926172116-812a484cc733 // indirect
+	github.com/jackc/pgx v3.6.2+incompatible // indirect
+	github.com/lib/pq v1.8.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/shopspring/decimal v1.2.0 // indirect
+	golang.org/x/crypto v0.0.0-20201117144127-c1f2f97bffc9 // indirect
+	golang.org/x/text v0.3.4 // indirect
+	gopkg.in/jackc/pgx.v3 v3.6.2
+)
---
+++ dbclient/v4/dbclient.go
@@ -0,0 +1,61 @@
+package dbclient
+
+import (
+	"context"
+	"fmt"
+	"github.com/HomesNZ/go-common/env"
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+const DefaultMaxConnect = 1
+
+type Config struct {
+	ServiceName string
+	Host        string
+	User        string
+	Name        string
+	Password    string
+	MaxConns    int
+	Port        int
+	SearchPath  string
+}
+
+// ConfigEnv returns config, all settings will be pulled from environment variables
+func ConfigFromEnv(serviceName string) Config {
+	return Config{
+		ServiceName: serviceName,
+		Host:        env.GetString("DB_HOST", "localhost"),
+		User:        env.GetString("DB_USER", "postgres"),
+		Name:        env.MustGetString("DB_NAME"),
+		Password:    env.GetString("DB_PASSWORD", ""),
+		Port:        env.GetInt("DB_PORT", 5432),
+		SearchPath:  env.GetString("DB_SEARCH_PATH", ""),
+		MaxConns:    DefaultMaxConnect,
+	}
+}
+
+// connectionConfig returns the database connection config and error
+func connectionConfig(cfg *Config) (*pgxpool.Config, error) {
+	connStr := fmt.Sprintf("host=%s user=%s dbname=%s password=%s port=%d pool_max_conns=%d search_path=%s application_name=%s",
+		cfg.Host,
+		cfg.User,
+		cfg.Name,
+		cfg.Password,
+		uint16(cfg.Port),
+		cfg.MaxConns,
+		cfg.SearchPath,
+		cfg.ServiceName,
+	)
+	config, err := pgxpool.ParseConfig(connStr)
+
+	return config, err
+}
+
+// Conn returns pgx connection pool and error
+func Conn(ctx context.Context, cfg *Config) (*pgxpool.Pool, error) {
+	config, err := connectionConfig(cfg)
+	if err != nil {
+		return nil, err
+	}
+	return pgxpool.ConnectConfig(ctx, config)
+}
