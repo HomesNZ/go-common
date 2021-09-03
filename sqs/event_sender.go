6227b74e298ab8b6345fe2c643ae0c84822aabef
Igor Rashyn <igor.rashyn@homes.co.nz>
2021-09-03 15:56:02 +1200

    added mock for event_sender

---
+++ sqs/event_sender.go
@@ -0,0 +1,57 @@
+package sqs
+
+import (
+	"sync"
+
+	"github.com/HomesNZ/go-common/sns"
+)
+
+type EventSender interface {
+	Send(ev EventTyper) error
+}
+
+type eventSender struct {
+	mu     sync.Mutex
+	topics map[string]*sns.Topic
+}
+
+func NewEventSender() EventSender {
+	return &eventSender{
+		topics: make(map[string]*sns.Topic),
+	}
+}
+
+func (e *eventSender) Send(ev EventTyper) error {
+	topic, err := e.initTopic(ev.EventType())
+	if err != nil {
+		return err
+	}
+	_, err = topic.PushMessage(ev)
+	return err
+}
+
+func (e *eventSender) initTopic(name string) (*sns.Topic, error) {
+	topic, ok := e.getTopic(name)
+	if ok {
+		return topic, nil
+	}
+	topic, err := sns.NewTopic(name)
+	if err != nil {
+		return nil, err
+	}
+	e.setTopic(name, topic)
+	return topic, nil
+}
+
+func (e *eventSender) getTopic(name string) (*sns.Topic, bool) {
+	e.mu.Lock()
+	defer e.mu.Unlock()
+	topic, ok := e.topics[name]
+	return topic, ok
+}
+
+func (e *eventSender) setTopic(name string, topic *sns.Topic) {
+	e.mu.Lock()
+	e.topics[name] = topic
+	e.mu.Unlock()
+}
