e5c3266282bc6632fd52d538ff8548660c5836a5
Igor <igor.rashyn@gmail.com>
2022-05-05 11:27:54 +1200

    sqs_v2 log update

--- redis/delete.go
+++ redis/delete.go
@@ -2,7 +2,6 @@ package redis
 
 import (
 	"github.com/gomodule/redigo/redis"
-	"github.com/sirupsen/logrus"
 )
 
 // Delete removes a key from redis and returns its value
@@ -12,13 +11,11 @@ func (c cache) Delete(key string) (string, error) {
 
 	reply, err := redis.String(conn.Do("GET", key))
 	if err != nil {
-		logrus.WithError(err).Error(err)
 		return "", err
 	}
 
 	_, err = conn.Do("DEL", key)
 	if err != nil {
-		logrus.WithError(err).Error(err)
 		return "", err
 	}
 
