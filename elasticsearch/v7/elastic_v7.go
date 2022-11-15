86c5dc4d6ea103e13a786757949796d657fddb31
Fabiano <fabianosoriani@gmail.com>
2022-11-16 11:25:33 +1300

    improves v7 error handling

--- elasticsearch/v7/elastic_v7.go
+++ elasticsearch/v7/elastic_v7.go
@@ -6,6 +6,7 @@ import (
 	"github.com/HomesNZ/env"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	v4 "github.com/aws/aws-sdk-go/aws/signer/v4"
+	"github.com/pkg/errors"
 	"github.com/sha1sum/aws_signing_client"
 	"github.com/sirupsen/logrus"
 
@@ -14,19 +15,24 @@ import (
 
 // Gets a connection to AWS OpenSearch - depends on ENV
 //
-//	ELASTICSEARCH_URLS
-//	+ AWS_ACCESS_KEY_ID or AWS_ACCESS_KEY
-//	+ AWS_SECRET_ACCESS_KEY or AWS_SECRET_KEY
-func Conn() *elastic.Client {
+//		ELASTICSEARCH_URLS
+//		+ AWS_ACCESS_KEY_ID or AWS_ACCESS_KEY
+//		+ AWS_SECRET_ACCESS_KEY or AWS_SECRET_KEY
+//	    + (optional) AWS_REGION
+func Conn() (*elastic.Client, error) {
 	log := logrus.WithField("package", "elasticsearch")
 	signer := v4.NewSigner(credentials.NewEnvCredentials())
-	awsClient, err := aws_signing_client.New(signer, nil, "es", "ap-southeast-2")
+	region := env.GetString("AWS_REGION", "ap-southeast-2")
+	awsClient, err := aws_signing_client.New(signer, nil, "es", region)
 	if err != nil {
-		log.Errorln("error creating aws signer")
-		panic(err)
+		return nil, errors.Wrap(err, "error creating aws signer")
 	}
 
-	urls := strings.Split(env.MustGetString("ELASTICSEARCH_URLS"), ";")
+	url_raw := env.GetString("ELASTICSEARCH_URLS", "")
+	if url_raw == "" {
+		return nil, errors.New("missing ELASTICSEARCH_URLS")
+	}
+	urls := strings.Split(url_raw, ";")
 	client, err := elastic.NewClient(
 		elastic.SetURL(urls...),
 		elastic.SetMaxRetries(10),
@@ -37,9 +43,8 @@ func Conn() *elastic.Client {
 		elastic.SetGzip(false),
 	)
 	if err != nil {
-		log.Errorln("error creating client")
-		panic(err)
+		return nil, errors.Wrap(err, "error creating client")
 	}
 	log.Infoln("using olivere/elastic/v7", urls)
-	return client
+	return client, nil
 }
