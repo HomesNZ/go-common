e62656dc158344c6ed37dd0da78d7c04f66ecc8d
wangsheng2888 <sheng.wang@trademe.co.nz>
2023-05-03 23:18:32 +1200

    add middleware lambda response header

---
+++ middleware/lambda/lambda.go
@@ -0,0 +1,50 @@
+package middleware_lambda
+
+import (
+	"context"
+	"errors"
+	"strings"
+
+	"github.com/HomesNZ/go-common/env"
+	"github.com/aws/aws-lambda-go/events"
+)
+
+var (
+	requestHeaderOriginEnv string
+	mapRequestHeaderOrigin = make(map[string]bool)
+)
+
+func init() {
+	requestHeaderOriginEnv = env.GetString("REQUEST_HEADER_ORIGIN", "")
+	for _, value := range strings.Split(requestHeaderOriginEnv, ";") {
+		mapRequestHeaderOrigin[value] = true
+	}
+}
+
+type LambdaHandler func(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error)
+
+func AddResponseHeaders(next LambdaHandler) LambdaHandler {
+	return func(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+		if requestHeaderOriginEnv == "" {
+			return events.APIGatewayProxyResponse{}, errors.New("REQUEST_HEADER_ORIGIN does not exist in env")
+		}
+
+		res, err := next(ctx, request)
+		if err != nil {
+			return res, err
+		}
+
+		requestHeaderOrigin := request.Headers["origin"]
+		if _, ok := mapRequestHeaderOrigin[requestHeaderOrigin]; ok {
+			requestHeaderOriginEnv = requestHeaderOrigin
+		}
+
+		header := map[string]string{
+			"Access-Control-Allow-Origin":      requestHeaderOriginEnv,
+			"Access-Control-Allow-Credentials": "true",
+		}
+
+		res.Headers = header
+		return res, nil
+	}
+}
